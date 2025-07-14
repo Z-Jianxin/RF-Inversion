8d35fcdb876df2b8546d897957fceae4f7d5502d
Jianxin Zhang <jianxinz@umich.edu>
2025-07-14 23:15:57 +0000

    added lpips

--- gradio_demo_rfinversion.py
+++ gradio_demo_rfinversion.py
@@ -7,6 +7,8 @@ import numpy as np
 from pipeline_rf_inversion_sde import RFInversionFluxPipelineSDE
 
 from transformers import CLIPProcessor, CLIPModel
+import lpips
+import torchvision.transforms as transforms
 
 class FluxEditor:
     def __init__(self):
@@ -16,6 +18,12 @@ class FluxEditor:
         )
         self.clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32") # it's lightweighted so it can live on CPU
         self.clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        self.lpips = lpips.LPIPS(net='vgg').to('cuda')
+        self.lpips_transform = transforms.Compose([
+            transforms.Resize((1024, 1024)),  # resize for consistency (optional)
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))  # LPIPS expects inputs in [-1, 1]
+        ])
 
     def print_clip_score(self, image, prompt):
         clip_inputs = self.clip_processor(
@@ -77,8 +85,6 @@ class FluxEditor:
             edited_image.convert("RGB")
         )
         self.pipe = self.pipe.to("cpu")
-        torch.cuda.empty_cache()
-        print("End Edit")
 
         print("target prompt vs target image: ")
         self.print_clip_score(edited_image, target_prompt)
@@ -95,6 +101,13 @@ class FluxEditor:
         mse_median = np.median((arr1 - arr2) ** 2)
         print("L1 Distance:", mae, "median:", mae_median)
         print("L2 Distance:", mse, "median:", mse_median)
+
+        
+        with torch.no_grad():
+            dist = self.lpips(self.lpips_transform(init_resized).to("cuda"), self.lpips_transform(edited_image).to("cuda"))
+        print("LPIPS distance: ", dist.item())
+        torch.cuda.empty_cache()
+        print("End Edit\n\n")
         return edited_image, diff
 
 
